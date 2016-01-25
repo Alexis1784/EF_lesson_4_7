5b43c4a76ab81618d10b487482b1b31b4b508294
Alex <alexis1784@gmail.com>
2016-01-25 17:17:39 +0500

    working version

--- EF_lesson_4_3/Program.cs
+++ EF_lesson_4_3/Program.cs
@@ -12,9 +12,31 @@ namespace EF_lesson_4_3
         {
             using (PhoneContext db = new PhoneContext())
             {
-                var phones = db.Phones.Where(p => p.Company.Name == "Samsung");
-                foreach (Phone p in phones)
-                    Console.WriteLine("{0}.{1} - {2}", p.Id, p.Name, p.Price);
+                int number1 = db.Phones.Count();
+                // найдем кол-во моделей, которые в названии содержат Samsung
+                int number2 = db.Phones.Count(p => p.Name.Contains("Samsung"));
+
+                Console.WriteLine(number1);
+                Console.WriteLine(number2);
+
+                int minPrice = db.Phones.Min(p => p.Price);
+                // максимальная цена
+                int maxPrice = db.Phones.Max(p => p.Price);
+                // средняя цена на телефоны фирмы Samsung
+                double avgPrice = db.Phones.Where(p => p.Company.Name == "Samsung")
+                                    .Average(p => p.Price);
+
+                Console.WriteLine(minPrice);
+                Console.WriteLine(maxPrice);
+                Console.WriteLine(avgPrice);
+
+                // суммарная цена всех моделей
+                int sum1 = db.Phones.Sum(p => p.Price);
+                // суммарная цена всех моделей фирмы Samsung
+                int sum2 = db.Phones.Where(p => p.Name.Contains("Samsung"))
+                                    .Sum(p => p.Price);
+                Console.WriteLine(sum1);
+                Console.WriteLine(sum2);
             }
             Console.ReadLine();
         }
